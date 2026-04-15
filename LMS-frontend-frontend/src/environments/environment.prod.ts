a82543f2f916594b5d5da4850752fd198e8ea1ed
Gowtham862 <gowthamk712812@gmail.com>
2026-04-15 16:41:11 +0530

    changedp

--- LMS-frontend-frontend/src/environments/environment.prod.ts
+++ LMS-frontend-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://lms-backend-service:8080'
+  apiBaseUrl: 'http://34.93.144.161:8080'
 };
--- LMS-frontend-frontend/src/environments/environment.ts
+++ LMS-frontend-frontend/src/environments/environment.ts
@@ -4,9 +4,9 @@ export const environment = {
   // imagebaseurl:'http://192.168.11.124:8080/uploads/',
   // trainerimage:'http://192.168.11.124:8080/'
 
-   apiBaseUrl: 'http://lms-backend-service:8080',
-  imagebaseurl:'http://lms-backend-service/uploads/',
-  trainerimage:'http://lms-backend-service:8080/'
+   apiBaseUrl: 'http://34.93.144.161:8080',
+  imagebaseurl:'http://34.93.144.161/uploads/',
+  trainerimage:'http://34.93.144.161:8080/'
 
   //    apiBaseUrl: 'http://192.168.11.105:8080',
   // imagebaseurl:'http://192.168.11.105:8080/uploads/',
